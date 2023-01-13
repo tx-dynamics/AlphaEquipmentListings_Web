63796954d4dc693c1316f6c46de0458ad24c896f
Abdul rehman <abdulrehman.dev@techxpert.io>
2023-01-13 21:20:07 +0500

    update index.js

--- src/pages/viewers/index.js
+++ src/pages/viewers/index.js
@@ -1,6 +1,5 @@
 export { default as SignIn } from './signIn/SignIn'
 export { default as SignUp } from './signUp/SignUp'
-export { default as OtpScreen } from './otpScreen/OtpScreen'
 export { default as ForgotPassword } from './forgotPassword/ForgotPassword'
 export { default as CreatePassword } from './createPassword/CreatePassword'
 export { default as HomePage } from './homePage/HomePage'
@@ -9,3 +8,4 @@ export { default as ProductListingPage } from './productListingPage/ProductListi
 export { default as ProductDetailPage } from './productDetailPage/ProductDetailPage'
 export { default as Calculator } from './calculator/Calculator'
 export { default as Financing } from './financing/Financing'
+export { default as OtpScreen } from './otpScreen/OtpScreen'
